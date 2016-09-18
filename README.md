ffb9b9679878a93b9a2bb60b0929386ec5f1821c
Richard Saunders <richard@sdevsolutions.com>
2016-09-18 23:15:21 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# react-es2015
+
+Demo react application using es2015 syntax
+```
+npm install
+npm start
+```
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+export default {
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    debug: true,
+    devtool: 'eval-source-map',
+    entry: [
+        './src/index.js'
+    ],
+    target: 'web',
+    output: {
+        path: __dirname + 'dist',
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    plugins: [
+    ],
+    module: {
+        loaders: [
+            {test: /\.jsx?$/, exclude: /node_modules/, loaders: ['babel-loader']},
+            {test: /\.eot(\?v=\d+.\d+.\d+)?$/, loader: 'file'},
+            {test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url?limit=10000&mimetype=application/font-woff"},
+            {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
+            {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'},
+            {test: /\.(jpe?g|png|gif)$/i, loader: 'file?name=[name].[ext]'},
+            {test: /\.ico$/, loader: 'file?name=[name].[ext]'},
+            {test: /(\.css|\.scss)$/, loaders: ['style', 'css?sourceMap', 'postcss', 'sass?sourceMap']}
+        ]
+    }
+};
