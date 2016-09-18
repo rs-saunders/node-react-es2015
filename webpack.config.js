8d0ee5c2555edf868a5b425077069bef27d16344
Richard Saunders <richard@sdevsolutions.com>
2016-09-19 00:10:23 +0100

    added linting

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ export default {
     debug: true,
     devtool: 'eval-source-map',
     entry: [
-        './src/index.js'
+        './src/index.jsx'
     ],
     target: 'web',
     output: {
@@ -19,7 +19,7 @@ export default {
         loaders: [
             {test: /\.jsx?$/, exclude: /node_modules/, loaders: ['babel-loader']},
             {test: /\.eot(\?v=\d+.\d+.\d+)?$/, loader: 'file'},
-            {test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url?limit=10000&mimetype=application/font-woff"},
+            {test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url?limit=10000&mimetype=application/font-woff'},
             {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
             {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'},
             {test: /\.(jpe?g|png|gif)$/i, loader: 'file?name=[name].[ext]'},
