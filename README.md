9ca1f3b3ac07426f2ff5a36768e3cfb0267e67c7
Richard Saunders <richard@sdevsolutions.com>
2016-09-29 07:40:53 +0100

    Broke league box up into components

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # react-es2015
 
 Demo react application using es2015 syntax
+
+WORK IN PROGRESS
 ```
 npm install
 npm start
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,120 +1,66 @@
 import React from 'react';
 
+const LeageBoxRow = (props) => {
+
+    const letter = String.fromCharCode(65 + +props.index);
+    let isPlayerRow;
+    const scoreBoxes = [];
+    for(let i = 1; i <= props.boxCount; i++) {
+        isPlayerRow = ((i-1) == props.index);
+        const className = isPlayerRow ? 'score-box black' : 'score-box';
+        scoreBoxes.push(<td key={i} className={className}></td>);
+    }
+    return (
+        <tr>
+            <td>{props.name}</td>
+            <td>{props.tel1}</td>
+            <td>{props.tel2}</td>
+            <td>{props.email}</td>
+            <td className="letter-box">{letter}</td>
+            {scoreBoxes}
+            <td className="bp-box"></td>
+            <td className="total-box"></td>
+        </tr>
+    );
+};
+
+const LeagueBox = () => {
+    return (
+        <table className="league-box">
+            <thead>
+            <tr>
+                <th className="name">Name</th>
+                <th className="tel">Tel 1</th>
+                <th className="tel">Tel 2</th>
+                <th className="email">Email</th>
+                <th></th>
+                <th className="letter-box">A</th>
+                <th className="letter-box">B</th>
+                <th className="letter-box">C</th>
+                <th className="letter-box">D</th>
+                <th className="letter-box">E</th>
+                <th className="letter-box">F</th>
+                <th className="bp-box">BP</th>
+                <th className="total-box">Total</th>
+            </tr>
+            </thead>
+            <tbody>
+                <LeageBoxRow key="0" index="0" boxCount="6" name="Richard Saunders" tel1="00000 000 000" tel2="" email="richard@sdevsolutions.com" />
+                <LeageBoxRow key="1" index="1" boxCount="6" name="John Doe" tel1="00000 000 000" tel2="00000 000 000" email="xxxx@xxxxx.xxx" />
+                <LeageBoxRow key="2" index="2" boxCount="6" name="Bruce Banner" tel1="00000 000 000" tel2="" email="xxxx@xxxxx.xxx" />
+                <LeageBoxRow key="3" index="3" boxCount="6" name="Stephen Strange" tel1="00000 000 000" tel2="00000 000 000" email="xxxx@xxxxx.xxx" />
+                <LeageBoxRow key="4" index="4" boxCount="6" name="Tony Stark" tel1="00000 000 000" tel2="" email="xxxx@xxxxx.xxx" />
+                <LeageBoxRow key="5" index="5" boxCount="6" name="Bruice Wayne" tel1="00000 000 000" tel2="" email="" />
+            </tbody>
+        </table>
+    );
+};
+
 const HomePage = () => {
     return (
         <div>
             <h1>Home</h1>
-            <table className="league-box">
-                <thead>
-                    <tr>
-                        <th className="name">Name</th>
-                        <th className="tel">Tel 1</th>
-                        <th className="tel">Tel 2</th>
-                        <th className="email">Email</th>
-                        <th></th>
-                        <th className="letter-box">A</th>
-                        <th className="letter-box">B</th>
-                        <th className="letter-box">C</th>
-                        <th className="letter-box">D</th>
-                        <th className="letter-box">E</th>
-                        <th className="letter-box">F</th>
-                        <th className="bp-box">BP</th>
-                        <th className="total-box">Total</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Richard Saunders</td>
-                        <td>00000 000 000</td>
-                        <td></td>
-                        <td>richard@sdevsolutions.com</td>
-                        <td className="letter-box">A</td>
-                        <td className="score-box black"></td>
-                        <td className="score-box"></td>
-                        <td className="score-box"></td>
-                        <td className="score-box"></td>
-                        <td className="score-box"></td>
-                        <td className="score-box"></td>
-                        <td className="bp-box"></td>
-                        <td className="total-box"></td>
-                    </tr>
-                    <tr>
-                        <td>John Doe</td>
-                        <td>00000 000 000</td>
-                        <td>00000 000 000</td>
-                        <td>xxxx@xxxxx.xxx</td>
-                        <td className="letter-box">B</td>
-                        <td></td>
-                        <td className="score-box black"></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td className="bp-box"></td>
-                        <td className="total-box"></td>
-                    </tr>
-                    <tr>
-                        <td>Bruce Banner</td>
-                        <td>00000 000 000</td>
-                        <td></td>
-                        <td>xxxx@xxxxx.xxx</td>
-                        <td className="letter-box">C</td>
-                        <td></td>
-                        <td></td>
-                        <td className="score-box black"></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td className="bp-box"></td>
-                        <td className="total-box"></td>
-                    </tr>
-                    <tr>
-                        <td>Stephen Strange</td>
-                        <td>00000 000 000</td>
-                        <td>00000 000 000</td>
-                        <td>xxxx@xxxxx.xxx</td>
-                        <td className="letter-box">D</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td className="score-box black"></td>
-                        <td></td>
-                        <td></td>
-                        <td className="bp-box"></td>
-                        <td className="total-box"></td>
-                    </tr>
-                    <tr>
-                        <td>Tony Stark</td>
-                        <td>00000 000 000</td>
-                        <td></td>
-                        <td>xxxx@xxxxx.xxx</td>
-                        <td className="letter-box">E</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td className="score-box black"></td>
-                        <td></td>
-                        <td className="bp-box"></td>
-                        <td className="total-box"></td>
-                    </tr>
-                    <tr>
-                        <td>Bruce Wayne</td>
-                        <td>00000 000 000</td>
-                        <td></td>
-                        <td>xxxx@xxxxx.xxx</td>
-                        <td className="letter-box">F</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td className="score-box black"></td>
-                        <td className="bp-box"></td>
-                        <td className="total-box"></td>
-                    </tr>
-                </tbody>
-            </table>
+            <LeagueBox />
         </div>
     );
 };
